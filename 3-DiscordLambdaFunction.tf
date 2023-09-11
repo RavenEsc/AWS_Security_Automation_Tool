353bb4d08b5d0c007ad89ab3b801de25becd29cb
Raven Spencer <107158921+RavenEsc@users.noreply.github.com>
2023-09-11 16:17:16 -0500

    changed it to Arch.. instead of compat..

--- 3-DiscordLambdaFunction.tf
+++ 3-DiscordLambdaFunction.tf
@@ -49,7 +49,7 @@ module "lambda_layer_discord" {
   layer_name          = "lambda-layer-discord"
   description         = "lambda layer "
   compatible_runtimes = [var.py_runtime]
-  compatible_architectures = "x86_64"
+  architectures = ["x86_64"]
 
   source_path = "python.zip"
 }
