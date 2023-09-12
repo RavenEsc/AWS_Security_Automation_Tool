91234b232da5fd4e9bf4ee8aa6c8d6bc3bd813c4
Raven Spencer <107158921+RavenEsc@users.noreply.github.com>
2023-09-12 18:15:14 -0500

    DateTime json fix

--- index.py
+++ index.py
@@ -2,9 +2,14 @@ import os
 import json
 import boto3
 import traceback
+from datetime import datetime
 
 def lambda_handler(event, context):
-    try: # Get the list of EC2 instances
+    try:
+        # Get the current datetime
+        current_datetime = datetime.now().isoformat()
+
+        # Get the list of EC2 instances
         ec2 = boto3.client('ec2')
         instances = ec2.describe_instances()
 
@@ -19,16 +24,23 @@ def lambda_handler(event, context):
 
     except Exception as e:
         traceback_msg = traceback.format_exc()
-        return {'statusCode': 500,
-                'body': {"message": f"Error reading/listing EC2 instances: {str(e)}", 'traceback': traceback_msg}
-                }
-        # Code ends here if there is an error
+        return {
+            'statusCode': 500,
+            'body': {
+                "message": f"Error reading/listing EC2 instances: {str(e)}",
+                'traceback': traceback_msg
+            }
+        }
 
     # Check if the public_instances list is empty
     if not public_instances:
         # Return a status code 200 with a body 'No Results to publish'
         return {'statusCode': 200, 'body': 'No Results to publish'}
     else:
+        # Add the current datetime to the public_instances list
+        for instance in public_instances:
+            instance['CurrentDatetime'] = current_datetime
+
         # Publish the results to the SNS topic
         sns = boto3.client('sns')
         try:
@@ -44,5 +56,8 @@ def lambda_handler(event, context):
             traceback_msg = traceback.format_exc()
             return {
                 'statusCode': 500,
-                'body': {"message": f"Error publishing to SNS Topic: {str(e)}", 'traceback': traceback_msg}
+                'body': {
+                    "message": f"Error publishing to SNS Topic: {str(e)}",
+                    'traceback': traceback_msg
                 }
+            }
