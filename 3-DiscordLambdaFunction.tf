b349477700285c8687fb6512b5bb34634cd0a76e
Raven Spencer <107158921+RavenEsc@users.noreply.github.com>
2023-09-12 14:59:07 -0500

    create_package = false

--- 3-DiscordLambdaFunction.tf
+++ 3-DiscordLambdaFunction.tf
@@ -51,6 +51,8 @@ module "lambda_layer_discord" {
   compatible_runtimes = ["python3.9"]
   compatible_architectures = ["x86_64"]
 
-  source_path = "${path.module}/../python"
+  # source_path = "${path.module}/../python"
+  create_package = false
+  local_existing_package = "./python.zip"
 }
 
