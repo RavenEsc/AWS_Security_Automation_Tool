ef5f8138490663596d56eb9a0b74bf389a35dbac
Raven Spencer <107158921+RavenEsc@users.noreply.github.com>
2023-09-14 17:21:06 -0500

    commit to fallback

--- index2.py
+++ index2.py
@@ -6,41 +6,47 @@ import traceback
 def lambda_handler(event, context):
     try:
         # Get the new messages from the SQS Queue
-        records = event['Records']
-    except Exception as e:
-        traceback_message = traceback.format_exc()
-        return {
-            "statusCode": 500,
-            "body": {"message": f"Error processing SQS messages: {e}","traceback": traceback_message}
-        }
+        records = event.get('Records', [])  # Use get() to prevent KeyError
 
-    for record in records:
-        message = record['body']
-        
         # Create S3 client
-        s3 = boto3.client('s3')
+        s3 = boto3.client('s3')  # Move outside the loop for efficiency
 
-        # Create time variables
-        current_time = datetime.datetime.now()
-        current_date = datetime.date.today()
-    try:
-        # Create a file in S3 bucket with the message as content
-        bucket_name = os.getenv('buck_lm')
-        folder_path = f"{current_date}/"
-        file_name = f"{folder_path}{current_time.strftime('%H:%M:%S')}-ec2pubcheck.json"
-        s3.put_object(
-            Body=message,
-            Bucket=bucket_name,
-            Key=file_name
-        )
+        # Encode message using desired encoding (e.g., 'utf-8')
+        message_bytes = message.encode('utf-8')
+        
+        for record in records:
+            message = record['body']
+            
+            # Create time variables (current_time and current_date)
+            current_time = datetime.datetime.now()
+            current_date = datetime.date.today()
+
+            try:
+                # Create a file in S3 bucket with the message as content
+                bucket_name = os.getenv('buck_lm')
+                folder_path = f"{current_date}/"
+                file_name = f"{folder_path}{current_time.strftime('%H:%M:%S')}-ec2pubcheck.json"
+                
+                s3.put_object(
+                    Body=message_bytes,
+                    Bucket=bucket_name,
+                    Key=file_name
+                )
+            except Exception as e:
+                traceback_message = traceback.format_exc()
+                return {
+                    "statusCode": 500,
+                    "body": {"message": f"Error creating file in S3 bucket: {e}", "traceback": traceback_message}
+                }
+
     except Exception as e:
         traceback_message = traceback.format_exc()
         return {
             "statusCode": 500,
-            "body": {"message": f"Error creating file in S3 bucket: {e}", "traceback": traceback_message}
+            "body": {"message": f"Error processing SQS messages: {e}", "traceback": traceback_message}
         }
-    
+
     return {
         "statusCode": 200,
-        "body": {"message": "Messages transferred to S3 successfully"}
+        "body": "Messages transferred to S3 successfully"
     }
--- index3.py
+++ index3.py
@@ -1,4 +1,3 @@
-import discord_webhook
 from discord_webhook import DiscordWebhook, DiscordEmbed
 import traceback
 
@@ -9,25 +8,25 @@ def lambda_handler(event, context):
         records = event['Records']
 
         for record in records:
-            message = record['body']
+            message = record['body'] # Assume record['body'] is a string, not a dictionary
 
             try:
                 # Sends the Public EC2 Alert Notification!
-                if message['Alert'] == "Public_EC2_Instance":
-                    # Create a DiscordWebhook object with the URL of the Discord webhook
-                    webhook = discord_webhook.DiscordWebhook(url="https://discordapp.com/api/webhooks/1147701063630196786/PVU9g477tn2u9ko0LZ5uTg4SUoQGqe_iSftGdhjZi1Szz5aIDDEew4soEPL80S3EYizy")
+                # if "Alert" in message and message['Alert'] == "Public_EC2_Instance":
+                # Create a DiscordWebhook object with the URL of the Discord webhook
+                webhook = DiscordWebhook(url="https://discordapp.com/api/webhooks/1147701063630196786/PVU9g477tn2u9ko0LZ5uTg4SUoQGqe_iSftGdhjZi1Szz5aIDDEew4soEPL80S3EYizy")
 
-                    # Create a DiscordEmbed object to define the content of the message
-                    discord_message = f"Instance ID: {message['ID']}\nPublic IP: {message['Alert']['Public_IP']}\nAttachment Time: {message['Alert']['Time_Created']}"
-                    embed = discord_webhook.DiscordEmbed(
-                        title="Public EC2 Instance!",
-                        description=f"{discord_message}\n{message}\n:)",
-                        color="03b2f8"
-                    )
-                    webhook.add_embed(embed)
-                    webhook.execute()
+                # Create a DiscordEmbed object to define the content of the message
+                discord_message = f"Instance ID: {message['ID']}\nPublic IP: {message['Public_IP']}\nAttachment Time: {message['Time_Created']}"
+                embed = DiscordEmbed(
+                    title="Public EC2 Instance!",
+                    description=discord_message,
+                    color=0x03b2f8 # Use an integer value for color
+                )
+                webhook.add_embed(embed)
+                webhook.execute() # Move this line inside the try block
 
-                # Raises an error message and traceback if an error in recieving the messages occurs
+                # Raises an error message and traceback if an error in receiving the messages occurs
             except Exception as e:
                 traceback_message = traceback.format_exc()
                 return {
