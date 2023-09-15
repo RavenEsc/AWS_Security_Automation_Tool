9fa5a43d4198207d34c106aeac00dc49c38fff05
Raven Spencer <107158921+RavenEsc@users.noreply.github.com>
2023-09-14 19:53:38 -0500

    added if statement for scalability

--- index3.py
+++ index3.py
@@ -14,30 +14,35 @@ def lambda_handler(event, context):
         for record in records:
             message = json.loads(record['body'])
             for item in message:
-                id = item['ID']
-                public_ip = item['Public_IP']
-                time_created = item['Time_Created']
-                
-                try:
-                    # Sends the message formatted to be easily readable to Discord channel webhook integration
-                    webhook = DiscordWebhook(url="https://discordapp.com/api/webhooks/1147701063630196786/PVU9g477tn2u9ko0LZ5uTg4SUoQGqe_iSftGdhjZi1Szz5aIDDEew4soEPL80S3EYizy")
-                    embed = DiscordEmbed(
-                        title="Public EC2 Instance!",
-                        description=f"Instance ID: {id}\nPublic IP: {public_ip}\nAttachment Time: {time_created}",
-                        color=0x03b2f8
-                    )
-                    webhook.add_embed(embed)
-                    webhook.execute()
+                # Alert Value so far between 'EC2_Public_Instance', 
+                alert = item['Alert']
 
-                # Handles the errors for sending the messages to Discord
-                except Exception as e:
-                    traceback_message = traceback.format_exc()
-                    logger.error(f"Error sending Discord message: {e}")
-                    logger.error(traceback_message)
-                    logger.error(message)
-                    return {
-                        "statusCode": 500,
-                        "body": {"message": f"Error sending Discord message: {e}"}
+                if alert == 'EC2_Public_Instance':
+                    # EC2 values set as variables
+                    id = item['ID']
+                    public_ip = item['Public_IP']
+                    time_created = item['Time_Created']
+                    
+                    try:
+                        # Sends the PublicEC2 Alert message formatted to be easily readable to Discord channel webhook integration
+                        webhook = DiscordWebhook(url="https://discordapp.com/api/webhooks/1147701063630196786/PVU9g477tn2u9ko0LZ5uTg4SUoQGqe_iSftGdhjZi1Szz5aIDDEew4soEPL80S3EYizy")
+                        embed = DiscordEmbed(
+                            title="Public EC2 Instance!",
+                            description=f"Instance ID: {id}\nPublic IP: {public_ip}\nAttachment Time: {time_created}",
+                            color=0x03b2f8
+                        )
+                        webhook.add_embed(embed)
+                        webhook.execute()
+
+                    # Handles the errors for sending the messages to Discord
+                    except Exception as e:
+                        traceback_message = traceback.format_exc()
+                        logger.error(f"Error sending Discord message: {e}")
+                        logger.error(traceback_message)
+                        logger.error(message)
+                        return {
+                            "statusCode": 500,
+                            "body": {"message": f"Error sending Discord message: {e}"}
                     }
     # Handles the errors for recieving the messages to Discord
     except Exception as e:
