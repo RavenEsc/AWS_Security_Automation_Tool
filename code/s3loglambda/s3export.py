e24d3837f46276d34fb434969673f2dad1201811
Raven Spencer <107158921+RavenEsc@users.noreply.github.com>
2023-10-02 23:36:57 -0500

    added icounter and renaming each alert as it's own

--- code/s3loglambda/s3export.py
+++ code/s3loglambda/s3export.py
@@ -3,6 +3,7 @@ import os
 import datetime
 import boto3
 import traceback
+import json
 
 # Initialize logger
 logger = logging.getLogger()
@@ -18,48 +19,57 @@ def lambda_handler(event, context):
         
         for record in records:
             message = record['body']
+            alertmessage = json.loads(record['body'])
 
             # Create time variables (current_time and current_date)
             current_time = datetime.datetime.now()
             current_date = datetime.date.today()
-            # Alert Value 
-            alert = list(message['Alert'])
-            try:
-                # Create a file in S3 bucket with the message as content
-                bucket_name = os.getenv('buck_lm')
-                folder_path = f"{current_date}/"
-                file_name = f"{folder_path}{current_time.strftime('%H:%M:%S')}-{alert}.json"
-                
-                s3.put_object(
-                    Body=message,
-                    Bucket=bucket_name,
-                    Key=file_name
-                )
-
-            # Handles the errors for creating the output log file in the S3 bucket
-            except Exception as e:
-                traceback_message = traceback.format_exc()
-                logger.error(f"Error creating file in S3 bucket: {e}")
-                logger.error(traceback_message)
-                logger.error(message)
-                return {
-                    "statusCode": 500,
-                    "body": {"message": f"Error creating file in S3 bucket: {e}"}
-                }
+            print(list(alertmessage))
             
-    # Handles the errors for recieving the messages
+            counter = 1  # Initialize counter variable
+            
+            for alerts in alertmessage:
+                alert = alerts['Alert']
+                print(alert)
+                try:
+                    # Create a file in S3 bucket with the message as content
+                    bucket_name = os.getenv('buck_lm')
+                    folder_path = f"{current_date}/"
+                    file_name = f"{folder_path}{current_time.strftime('%H:%M:%S')}-{alert}-{counter}.json"
+                    
+                    s3.put_object(
+                        Body=message,
+                        Bucket=bucket_name,
+                        Key=file_name
+                    )
+                    
+                    counter += 1  # Increment counter variable for each iteration
+    
+                # Handles the errors for creating the output log file in the S3 bucket
+                except Exception as e:
+                    traceback_message = traceback.format_exc()
+                    logger.error(f"Error creating file in S3 bucket: {e}")
+                    logger.error(traceback_message)
+                    logger.error(message)
+                    return {
+                        "statusCode": 500,
+                        "body": {"message": f"Error creating file in S3 bucket: {e}"}
+                    }
+                    
+                    
+    # Handles the errors for receiving the messages
     except Exception as e:
         traceback_message = traceback.format_exc()
-        logger.error(f"Error recieving SQS message(s): {e}")
+        logger.error(f"Error receiving SQS message(s): {e}")
         logger.error(traceback_message)
         logger.error(message)
         return {
             "statusCode": 500,
-            "body": {"message": f"Error recieving SQS message(s): {e}"}
+            "body": {"message": f"Error receiving SQS message(s): {e}"}
         }
 
     # Code Ends, notifies code worked properly
     return {
         "statusCode": 200,
-        "body": "Messages transferred to S3 successfully"
+        "body": {"message": "Messages transferred to S3 successfully"}
     }
