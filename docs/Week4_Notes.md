dcee73e3f6408431af71dd2459dcdb68ba2b7599
Raven Spencer <107158921+RavenEsc@users.noreply.github.com>
2023-10-09 18:59:48 -0500

    Test err, updated to aws:V5.20.0 & lamodule:V6.0.1

---
+++ docs/Week4_Notes.md
@@ -0,0 +1,25 @@
+# Project SAT
+
+Update: Finished up the six Goals set in week 3
+
+Applied to the Associate HanaByte Program.
+
+Find a fix in GitHub Actions to change the lambda python RunTime after the terraform is completed running
+
+- Find out how to run a command after a terraform workflow with VCS connection is completed
+
+Otherwise, Work on running a check on all RESOURCE policies for the wildcard(*) character/ overly permissive policy permission
+
+## Meeting
+
+- Make an AWS Org from Terraform
+- Landing Zone: AWS Accounts auto, predeployed, monitoring
+
+- Creating a new AWS account
+
+- Kuberenetes, A Whole New World, & Amazon EKS
+- A container orchestration conductor
+
+____________________
+
+Update: I can not use GitHub Actions with Terraform VCS, and the current lambda module is not compatible with the Version Control TFC. So the only current option is to create a local module. Time to learn how to make a module!
--- tf/3-DiscordLambdaFunction.tf
+++ tf/3-DiscordLambdaFunction.tf
@@ -1,60 +1,61 @@
 module "lambda_Discord" {
   source = "terraform-aws-modules/lambda/aws"
-  version       = "6.0.0"
+  version       = "6.0.1"
   function_name = "lambda-sat-discord"
   description   = "Sends messages as notifications from the SQS Queue to Discord Webhook Bot"
   handler       = "discordnote.lambda_handler"
-  runtime       = "python3.8"
+  runtime       = "python3.11"
   source_path = [
         {
             path = "../code/discordlambda"
             pip_requirements = true
         }
   ]
-
-attach_policy_json = true
-policy_json = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "AllowLambdaDisSQSAccess",
-      "Effect": "Allow",
-      "Action": [
-        "sqs:ReceiveMessage",
-        "sqs:DeleteMessage",
-        "sqs:GetQueueAttributes"
-      ],
-      "Resource": "${aws_sqs_queue.orders_to_notify.arn}"
-    }
-  ]
-}
-EOF
-
-  event_source_mapping = {
-    sqs = {
-      event_source_arn = aws_sqs_queue.orders_to_notify.arn
-    }
-  }
-
-  # layers = [
-  #   module.lambda_layer_discord.lambda_layer_arn,
-  # ]
-
-  tags = {
-    Name = "my-lambda-discord"
-  }
 }
 
-# module "lambda_layer_discord" {
-#   source = "terraform-aws-modules/lambda/aws"
 
-#   create_layer = true
+# attach_policy_json = true
+# policy_json = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Sid": "AllowLambdaDisSQSAccess",
+#       "Effect": "Allow",
+#       "Action": [
+#         "sqs:ReceiveMessage",
+#         "sqs:DeleteMessage",
+#         "sqs:GetQueueAttributes"
+#       ],
+#       "Resource": "${aws_sqs_queue.orders_to_notify.arn}"
+#     }
+#   ]
+# }
+# EOF
 
-#   layer_name          = "lambda-layer-discord"
-#   description         = "lambda layer "
-#   compatible_runtimes = [var.py_runtime]
+#   event_source_mapping = {
+#     sqs = {
+#       event_source_arn = aws_sqs_queue.orders_to_notify.arn
+#     }
+#   }
 
-#   source_path = "Discord-Webhook-Dependencies.zip"
+#   # layers = [
+#   #   module.lambda_layer_discord.lambda_layer_arn,
+#   # ]
+
+#   tags = {
+#     Name = "my-lambda-discord"
+#   }
 # }
 
+# # module "lambda_layer_discord" {
+# #   source = "terraform-aws-modules/lambda/aws"
+
+# #   create_layer = true
+
+# #   layer_name          = "lambda-layer-discord"
+# #   description         = "lambda layer that will build the dependencies upon deploy, without leaving a trace of the .zip file"
+# #   compatible_runtimes = [var.py_runtime]
+
+# #   source_path = "Discord-Webhook-Dependencies.zip"
+# # }
